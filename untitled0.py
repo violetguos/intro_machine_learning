4e99358fa9f04acb157aad8c6557312e876b6f67
proguorammer <vikuo@Violets-MacBook-Pro.local>
2017-09-26 14:11:24 -0400

    visualized

---
+++ untitled0.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 26 13:16:31 2017
+
+@author: vikuo
+"""
+
+from sklearn import datasets
+import matplotlib.pyplot as plt
+import numpy as np
+import random
+
+
+def load_data():
+    boston = datasets.load_boston()
+    X = boston.data
+    y = boston.target #y is the price
+    features = boston.feature_names
+    return X,y,features
+
+
+def visualize(X, y, features):
+    plt.figure(figsize=(20, 5))
+    feature_count = X.shape[1] #13 eatures
+    # i: index
+    for i in range(feature_count):
+        plt.subplot(3, 5, i + 1)
+        #TODO: Plot feature i against y
+        plt.plot(X[:,i], y, '.')
+    plt.tight_layout()
+    plt.show()
+
+
+def fit_regression(X,Y):
+    #TODO: implement linear regression
+    # Remember to use np.linalg.solve instead of inverting!
+    #raise NotImplementedError()
+    return 0
+
+def main():
+    # Load the data
+    X, y, features = load_data()
+    
+    print("Features: {}".format(features))
+    
+    # Visualize the features
+    visualize(X, y, features)
+    
+    #TODO: Split data into train and test
+    
+    # Fit regression model
+    w = fit_regression(X, y)
+
+    # Compute fitted values, MSE, etc.
+
+
+if __name__ == "__main__":
+    main()
+

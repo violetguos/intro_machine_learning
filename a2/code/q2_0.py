8f563cec3548f7888deab6348b15f2cb762f5727
proguorammer <yueguo598@gmail.com>
2017-10-29 18:34:51 -0400

    q2.0 done plot numbers

--- a2/code/q2_0.py
+++ a2/code/q2_0.py
@@ -10,17 +10,37 @@ import numpy as np
 import matplotlib.pyplot as plt
 #import matplotlib.pyplot.imshow
 
+def mean_i_digit(i_digits):
+    '''returns the mean for one digit,
+        avg across 700 samples for 64 pixels
+    '''
+    i_mean = np.zeros(64)
+    i_sum = np.sum(i_digits, axis = 0)
+    for i in range(0,64):
+        i_mean[i]=i_sum[i]/60.0
+    
+    #print i_mean
+    return i_mean
+
+
 
 def plot_means(train_data, train_labels):
     means = []
     for i in range(0, 10):
+        i_mean_matrix = np.zeros((8,8))
         i_digits = data.get_digits_by_label(train_data, train_labels, i)
         # Compute mean of class i
         #TODO: compute 64 by 64 matrix mean??
-        print type(i_digits)
-        means.append(np.mean(i_digits))
+        #700 row, 64 columns for each difit
+        #print i_digits[]
+        i_mean = mean_i_digit(i_digits) #imean is 64
+        #tes_list =i_mean.tolist()
+        #print len(tes_list)
+        i_mean_matrix = np.reshape(i_mean, (8,8))
+        means.append(i_mean_matrix)
+        #print means
     # Plot all means on same axis
-    all_concat = np.concatenate(means, 1)
+    all_concat = np.concatenate(means,1)
     plt.imshow(all_concat, cmap='gray')
     plt.show()
 
